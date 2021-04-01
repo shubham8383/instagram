f1193efbdd4e50b1d0574ed0e12a85c4a50aa5b3
shubham <shubham83834003@gmail.com>
2021-04-01 23:17:48 +0530

    firstcomit

---
+++ insta/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <div className="app__header">
+        <img
+        className="app__headerImage"
+        src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+        alt=""
+        />
+        
+        </div>
+        <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
